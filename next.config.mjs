66d29390028b32dac16de5d4e09c9658a78d7bdb
juanoned <juandiego.jd42@gmail.com>
2024-10-09 15:51:07 -0300

    Route-two

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        remotePatterns:[{protocol:"https",hostname:"static.wikia.nocookie.net"}]
+        remotePatterns:[{protocol:"https",hostname:"static.wikia.nocookie.net"},{protocol:"https",hostname:"disney.fandom.com"}]
     },
     rewrites:()=>{
         return[
             {
                 source:"/primeira-rota",
                 destination:"/route-one"
+            },
+            {
+                source:"/segunda-rota",
+                destination:"/route-two"
             }
         ]
     }
---
+++ src/app/route-two/page.tsx
@@ -0,0 +1,74 @@
+"use client"
+
+import { useEffect,useState,Suspense } from "react";
+import Image from "next/image";
+
+import { api } from "@/app/constants/api";
+
+
+const RouteTwo=()=>{
+    interface IData{
+        _id:string,
+        name:string,
+        imageUrl:string
+    }
+
+    const [name,setName] = useState<string>("")
+    const [page,setPage] = useState<string>("")
+    const [data,setData] = useState<IData[]>([])
+    const [err,setErr] = useState<boolean>(false)
+
+
+
+    useEffect(()=>{
+        api.get(`?name=${name}&page=${page}`).then((res)=>{
+            setData(res.data.data)
+            if(res.data.data.length===0){
+                setErr(true)
+            }else{
+                setErr(false)
+            }
+        }).catch((error)=>{
+            console.log(error)
+            if(error.response.status === 404){
+                setErr(true)
+            }
+            setErr(true)
+        })
+    },[name,page])
+
+    return(
+        <>
+        <div>
+            <div>
+                <label>Pagina:</label>
+                <input placeholder="1/149" value={page} onChange={(e)=>{setPage(e.target.value)}}/>
+            </div>
+            <div>
+                <label>Nome:</label>
+                <input value={name} onChange={(e)=>{setName(e.target.value)}}/>
+            </div>
+        </div>
+            {err&&<p>Não encontrado!!</p>}
+        <div className="flex flex-wrap justify-center">
+        {data.map((item)=>{
+          return(
+            <div key={item._id} className="p-1">
+              <div className="border-4">
+                  <div className="flex justify-center bg-gradient-to-r from-blue1 to-purple-500">
+                      <Image src={item.imageUrl} alt="" width={300} height={300}></Image>
+                  </div>
+                  <div className="flex justify-center">
+                      <p className="flex items-center text-center p-1">
+                          {item.name}
+                      </p>
+                  </div>
+              </div>
+          </div>
+        )})}
+        </div>
+        </>
+    )
+}
+
+export default RouteTwo;
